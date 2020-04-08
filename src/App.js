6d473236930cb7288070b5446c31e8ba81fc8332
your Shubinder Virk <shubinder92@gmail.com>
2020-04-08 11:04:42 +0100

    Added new features

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Home from './Components/Home'
 import Contact from './Components/Contact'
 import About from './Components/About'
 import Product from './Components/Products'
+import Navbar from'./Components/Navbar'
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as ROUTES from '../constants/routes'
+
+import Navigation from '../Navigation';
+// import './App.css/'
 
 const App = () => {
   return (
-   <fragment>
+   <fragment className="App">
+    <Router >
+      <Navbar />
+    </Router>
     <Home />
     <Product />
     <Images />
--- src/Components/pictures.js
+++ src/Components/pictures.js
@@ -8,23 +8,22 @@ const Images = () => {
       {Pictures.map((picture, index) => (
         <ol>
           <div className="Image">
-          <h3>Fragrance & Flicker</h3>
+            <h3>Fragrance & Flicker</h3>
             <img src={picture} className={`item${index + 1}`} />
             <div classname="Info">
               <h3>Price: £400</h3>
               <a
-        className="App-link"
-        href="https://unolusso.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Product Details
-      </a>
+                className="App-link"
+                href="https://unolusso.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Product Details
+              </a>
             </div>
           </div>
         </ol>
       ))}
-
     </div>
   );
 };
