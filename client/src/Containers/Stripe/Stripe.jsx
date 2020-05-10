20c19980a5bc400a41a4e4fd9c264f5b1083f127
your Shubinder Virk <shubinder92@gmail.com>
2020-05-10 06:43:32 +0100

    Adding the stripe payment to the backend

--- client/src/Containers/Stripe/Stripe.jsx
+++ client/src/Containers/Stripe/Stripe.jsx
@@ -1,4 +1,33 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout'
+import axios from axios;
 
-const StripeCheckoutButton = ({ price })
+const StripeCheckoutButton = ({ price }) => { 
+    const priceForStripe = price * 100; 
+    const publishableKey ='pl_test_kfeek'
+
+    const onToken = token => {
+        axios({
+            url:'payment',
+            method:'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert("Payment successful")
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your paymen. Please sure you use the provided credit card'
+            );
+        })
+    };
+
+    return (
+        <StripeCheckout 
+        label='Pay Now'
+        name=
+    )
+}
+
