89dd1372f80d033664680983490769d7b48593ed
your Shubinder Virk <shubinder92@gmail.com>
2020-06-23 12:54:06 +0100

    Deploying

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post("/send", (req, res) => {
   );
 });
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000)
 
 // listen(5000, () =>
 //   console.log(`Listening on port: 5000`)
