f9a2eb96b4410c51ba96f92e06d61486a8c89a96
your Shubinder Virk <shubinder92@gmail.com>
2020-06-23 12:30:32 +0100

    deploying

--- server.js
+++ server.js
@@ -113,6 +113,8 @@ app.post("/send", (req, res) => {
   );
 });
 
-app.listen(5000, () =>
-  console.log(`Listening on port: 5000`)
-);
+app.listen(process.env.PORT || 5000)
+
+// listen(5000, () =>
+//   console.log(`Listening on port: 5000`)
+// );
